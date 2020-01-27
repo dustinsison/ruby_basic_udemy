8015b85209c116640025de0f5b5cec573627b5e7
Dustin Sison <dustin.sison@moovweb.com>
2020-01-27 14:33:03 -0800

    Initial commit

---
+++ LessonFiles/ch1-2.rb
@@ -0,0 +1,23 @@
+## Lesson 14 - puts
+puts "Hello World"
+puts "I'm alive and well!"
+## Lesson 15 - print
+print "Hello "
+print "World"
+## Lesson 16 - p
+p "Hello"
+p "World"
+## Lesson 17 - Mathematics
+p 1 + 4
+p 10 - 6
+p 3 * 4
+p "School" + "bus"
+p 10 / 5
+p 12 / 5
+p 12.0 / 5
+p 12 / 5.0
+p 12.0 / 5.0
+p 2 * 2 * 2 * 2
+p 2**4 #("To the power of...")
+p 5 % 2 # Gives remainder from division (1)
+p 0.5/2
